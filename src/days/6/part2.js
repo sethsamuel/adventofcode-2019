e74d8e61d4ef833c3cf3caa4987d64ca8e35e58c
Seth Samuel <seth@itsovertime.com>
2019-12-21 11:52:41 -0500

    6/2

--- src/days/6/part2.js
+++ src/days/6/part2.js
@@ -1,56 +1,67 @@
+import _ from "lodash";
 self.onmessage = async e => {
   console.log("Message received", e);
   const { command, input } = e.data;
   if (command === "START") {
-    const [start, end] = input.split("-").map(i => parseInt(i));
-    let lastProgress = new Date().getTime();
+    const test = `COM)B
+    B)C
+    C)D
+    D)E
+    E)F
+    B)G
+    G)H
+    D)I
+    E)J
+    J)K
+    K)L
+    K)YOU
+    I)SAN`;
+    // const lines = test.split("\n").map(l => l.trim());
+    const lines = input.split("\n");
 
-    let matches = 0;
-    const isMatch = i => {
-      const digits = i
-        .toString()
-        .split("")
-        .map(c => parseInt(c));
-
-      let pairFound = false;
-      for (let offset = 0; offset < digits.length; offset++) {
-        if (digits[offset + 1] < digits[offset]) {
-          return false;
-        }
-        if (pairFound) {
-          continue;
-        }
-        if (digits[offset + 1] === digits[offset]) {
-          if (offset === 0) {
-            pairFound = digits[offset + 2] != digits[offset];
-          } else if (offset === digits.length - 2) {
-            pairFound = digits[offset - 1] != digits[offset];
-          } else {
-            pairFound =
-              digits[offset - 1] != digits[offset] &&
-              digits[offset + 2] != digits[offset];
-          }
-        }
-      }
-      return pairFound;
-    };
-    for (let i = start; i <= end; i++) {
-      if (new Date().getTime() - lastProgress > 1 / 30) {
-        lastProgress = new Date().getTime();
-
-        postMessage({
-          command: "PROGRESS",
-          complete: i - start,
-          total: end - start
-        });
-      }
-
-      if (isMatch(i)) {
-        matches++;
-      }
+    console.log(lines.sort());
+    const orbits = {};
+    for (let line of lines) {
+      const objects = line.split(")");
+      orbits[objects[0]] = orbits[objects[0]] || {
+        children: [],
+        ancestors: []
+      };
+      orbits[objects[1]] = orbits[objects[1]] || {
+        children: [],
+        ancestors: []
+      };
+      orbits[objects[0]].children = [
+        ...orbits[objects[0]].children,
+        objects[1]
+      ];
+      orbits[objects[1]].parent = objects[0];
     }
+    console.log(orbits);
+    let center = _.find(orbits, o => !o.parent);
+    console.log(center);
 
-    postMessage({ command: "RESULT", result: matches });
+    const buildAncestors = key => {
+      let parent = orbits[key].parent;
+      while (parent) {
+        orbits[key].ancestors.push(parent);
+        parent = orbits[parent].parent;
+      }
+    };
+    buildAncestors("YOU");
+    buildAncestors("SAN");
+    console.log(orbits["YOU"]);
+    console.log(orbits["SAN"]);
+    const fca = orbits["YOU"].ancestors.find(n =>
+      orbits["SAN"].ancestors.includes(n)
+    );
+    console.log("FCA", fca);
+
+    let distance =
+      orbits["YOU"].ancestors.indexOf(fca) +
+      orbits["SAN"].ancestors.indexOf(fca);
+
+    postMessage({ command: "RESULT", result: distance });
   }
 };
 
